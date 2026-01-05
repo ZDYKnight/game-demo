ec3ac94669dcb850a78621d73231b5b7d50cc57a
ZDYKnight <odenvon@outlook.com>
2026-01-06 00:43:21 +0800

    test

---
+++ _basd_.sh
@@ -0,0 +1,17 @@
+
+
+# 1. initital the git
+git init
+
+# 2. add the respots  name 
+git  remote add  origin https://github.com/ZDYKnight/bulkRNA-tut.git
+
+# 3. add the all  in  this repo
+git add .
+
+# 4. commit to  this change 
+git commit -m "Initial commit"
+
+# 5
+    git branch -M main 
+    git push -u origin main
